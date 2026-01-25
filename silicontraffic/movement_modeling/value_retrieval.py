96c4ba65416f21a1784a17faf912d68439403aef
硅硅草 <indexguo@163.com>
2026-01-25 18:40:26 +0800

    clear up: 删除多余文件

--- silicontraffic/movement_modeling/value_retrieval.py
+++
@@ -1,9 +0,0 @@
-from .movement import Movement
-from ..abstract_traffic_env_engine import TrafficEngine
-
-def get_movement_sum_queue_length(engine: TrafficEngine, movement: Movement) -> int:
-    """
-    Get the queue length of the given movement.
-    """
-    for lane in movement.from_lanes:
-        vehicles = engine.get_lane_vehicle_ids(lane)
