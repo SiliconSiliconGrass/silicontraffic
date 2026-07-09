f81a5956d9a66139410bac6d4286878365ae4af0
硅硅草 <indexguo@163.com>
2026-07-09 14:00:26 +0800

    check when import

--- silicontraffic/__init__.py
+++ silicontraffic/__init__.py
@@ -4,5 +4,19 @@
 This module provides a generic interface for SUMO and CityFlow.
 """
 
-from .ssumo import SiliconSumoEngine, load_sumo_road_net
-from .scityflow import SiliconCityFlowEngine, load_cityflow_road_net
+import logging
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+try:
+    from .ssumo import SiliconSumoEngine, load_sumo_road_net
+    logger.info("SUMO module found.")
+except (ImportError, ModuleNotFoundError):
+    logger.warning("SUMO module not found. Related features will not work available.")
+
+try:
+    from .scityflow import SiliconCityFlowEngine, load_cityflow_road_net
+    logger.info("CityFlow module found.")
+except (ImportError, ModuleNotFoundError):
+    logger.warning("CityFlow module not found. Related features will not work available.")
