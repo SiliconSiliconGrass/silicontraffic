a1c5f8bcc38947de09fdaf444f40ed67237ac7d8
硅硅草 <indexguo@163.com>
2026-07-11 22:10:03 +0800

    fix: fixed potential import errors in load_road_net()

--- silicontraffic/road_net/load_road_net.py
+++ silicontraffic/road_net/load_road_net.py
@@ -1,13 +1,13 @@
 from os import PathLike
 from typing import Literal
 from .road_net import RoadNet
-from ..ssumo import load_sumo_road_net
-from ..scityflow import load_cityflow_road_net
 
 def load_road_net(engine_type: Literal["sumo", "cityflow"], road_net_file_path: PathLike) -> RoadNet:
     if engine_type == "sumo":
+        from ..ssumo import load_sumo_road_net
         return load_sumo_road_net(road_net_file_path)
     elif engine_type == "cityflow":
+        from ..scityflow import load_cityflow_road_net
         return load_cityflow_road_net(road_net_file_path)
     else:
         raise ValueError(f"Unknown engine type: {engine_type}")
